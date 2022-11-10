dae541c80746a59147fa3b18be821a69871f6bbd
Achille DJAKPO <shwarzenegerachille@gmail.com>
2022-11-10 09:39:48 +0000

    add event blueprint to init file

--- o_evmt/__init__.py
+++ o_evmt/__init__.py
@@ -7,6 +7,7 @@ from o_evmt.routes.entreprise import entreprise_bp
 from o_evmt.routes.exposant import exposant_bp
 from o_evmt.routes.publication import publication_bp
 from o_evmt.routes.admin import admin_bp
+from o_evmt.routes.evenement import event_bp
 
 
 def create_app():
@@ -22,5 +23,6 @@ def create_app():
     app.register_blueprint(publication_bp)
     app.register_blueprint(entreprise_bp)
     app.register_blueprint(admin_bp)
+    app.register_blueprint(event_bp)
 
     return app
